e55b2220b48fbb974db64a994775adde3644ef52
cyrillotk <tatacyri1l@gmail.com>
2026-08-19 11:51:53 +0100

    testing form

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const expresslayouts = require("express-ejs-layouts");
+const app = express();
+
+app.set("view engine","ejs");
+app.use(expresslayouts);
+
+app.use(express.urlencoded({extended:true}));
+app.get("/form", (req,res) => {
+    res.render("form",{
+        title: "Registration"
+    });
+});
+
+app.post("/register", (req,res) => {
+    console.log(req.body);
+    res.send("registration successful");
+})
+
+
+
