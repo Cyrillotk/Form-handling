33cc9e342c464686bbd2f3ecc7d72ff96abf5270
cyrillotk <tatacyri1l@gmail.com>
2026-08-19 14:18:09 +0100

    including the POST/Redirect/Get pattern

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 const express = require("express");
+
 const app = express();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended:true}));
-app.get("/", (req,res) => {
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
     res.render("form");
 });
+
 app.post("/register", (req, res) => {
 
-    const name = req.body.name;
-    const email = req.body.email;
-    const message= req.body.message;
-
-    console.log(name);
-    console.log(email);
-    console.log(message);
-
-    res.render("result", {
-        name: name,
-        email: email,
-        message: message
-    });
-
-    app.post("/register", (req, res) => {
-
     const { name, email, message } = req.body;
+
 
     if (!name || !email || !message) {
         return res.send("Fill in all fields.");
     }
 
-    res.render("result", {
-        name,
-        email,
-        age
-    });
-    });
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    // Store/process the submitted data
+
+    // Redirect after POST
+    res.redirect("/success");
 });
-app.listen(3000,() => {
-    console.log("server running on port 3000");
+
+app.get("/success", (req, res) => {
+    res.render("result");
 });
 
-
-
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
